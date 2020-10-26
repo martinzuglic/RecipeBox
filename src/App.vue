08969ef0bc69c4d4aaaa686844c97abbee048f6d
Martin Žuglić <mzuglic@tvz.hr>
2020-10-26 09:17:46 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-table :data="data" :columns="columns"></b-table>
+    <AddRecipes />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddRecipes from './components/AddRecipes.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    AddRecipes
+  },
+  data() {
+            return {
+                data: [
+                    { 'id': 1, 'first_name': 'Jesse', 'last_name': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
+                    { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
+                    { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
+                    { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male' },
+                    { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female' }
+                ],
+                columns: [
+                    {
+                        field: 'id',
+                        label: 'ID',
+                        width: '40',
+                        numeric: true
+                    },
+                    {
+                        field: 'first_name',
+                        label: 'First Name',
+                    },
+                    {
+                        field: 'last_name',
+                        label: 'Last Name',
+                    },
+                    {
+                        field: 'date',
+                        label: 'Date',
+                        centered: true
+                    },
+                    {
+                        field: 'gender',
+                        label: 'Gender',
+                    }
+                ]
+            }
+        }
 }
 </script>
 
@@ -24,5 +60,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 600px;
+  margin: 60px auto;
 }
 </style>
---
+++ src/components/AddRecipes.vue
@@ -0,0 +1,22 @@
+<template>
+        <form>
+            <b-field label="Name">
+                <b-input v-model="name" placeholder="Upišite ime recepta"></b-input>
+            </b-field>
+            <b-button>New recipe</b-button>
+        </form>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
