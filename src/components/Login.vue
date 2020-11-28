99bd038439a7d5e7172551e3610716160b02a652
Martin Žuglić <mzuglic@tvz.hr>
2020-11-28 13:33:44 +0100

    New components

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page">
+    <div class="column" id="login">
+      <b-field label="Username" :label-position="labelPosition">
+        <b-input type="username"></b-input>
+      </b-field>
+
+      <b-field label="Password" :label-position="labelPosition">
+        <b-input type="password"></b-input>
+      </b-field>
+
+      <b-field id="post">
+        <p class="control">
+          <b-button>Login</b-button>
+        </p>
+      </b-field>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+
+  data() {
+    return {
+      labelPosition: "inside",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#login {
+  padding: 40px;
+  width: 500px;
+}
+</style>
